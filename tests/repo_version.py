45081dfac19eb52d4e6f0d7cdb1d25fcd18ac271
Jan Hutar <jhutar@redhat.com>
2019-08-02 22:57:26 +0200

    tests/repo_version.py: Refactored to the new structure

--- tests/repo_version.py
+++ tests/repo_version.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python3
 
-import sys
-import lib
+import logging
+import argparse
 
-def create_repo_version(repo,ver=None):
-    """creating repository version"""
-    return lib.post(repo+'versions/',data={'base_version': ver})
+import pulpperf.interact
+import pulpperf.structure
+import pulpperf.utils
+import pulpperf.reporting
 
-def get_repository_version(task):
-    """getting the repo version from the task"""
-    return lib.get(task)
+
+def create_repo_version(repo, ver):
+    """Create repository version based on different existing version"""
+    return pulpperf.interact.post(repo + 'versions/',
+                                  data={'base_version': ver})['_href']
+
 
 def create_repo(name):
     """Create repository"""
-    return lib.post('/pulp/api/v3/repositories/',data={'name': name})
+    return pulpperf.interact.post('/pulp/api/v3/repositories/',
+                                  data={'name': name})['_href']
 
-repo1 = create_repo(lib.get_random_string())
-repo1 = repo1.get("_href")
-repo2 = create_repo(lib.get_random_string())
-repo2 = repo2.get("_href")
 
-repo_ver_a = create_repo_version(repo1)
-task = repo_ver_a.get("task")
-version_a = get_repository_version(task)
-version_a = version_a.get("created_resources")
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create repository version copy",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    with pulpperf.structure.status_data(parser) as (args, data):
 
-repo_ver_b = create_repo_version(repo2,version_a)
-task = repo_ver_b.get("task")
-version_b = get_repository_version(task)
-version_b = version_b.get("created_resources")
-print (version_b)
+        for r in data:
+            r['repository_clone_name'] = pulpperf.utils.get_random_string()
+            r['repository_clone_href'] = create_repo(r['repository_name'])
+            logging.debug("For repo %s created repository clone %s" % (r['repository_href'], r['repository_clone_href']))
+
+        tasks = []
+        for r in data:
+            task = create_repo_version(r['repository_clone_href'], r['repository_version_href'])
+            logging.debug("Created version clone task %s" % task)
+            tasks.append(task)
+
+        results = pulpperf.interact.wait_for_tasks(tasks)
+        print(pulpperf.reporting.tasks_table(results))
+        print(pulpperf.reporting.tasks_min_max_table(results))
+        print("Version clone tasks waiting time:", pulpperf.reporting.tasks_waiting_time(results))
+        print("Version clone tasks service time:", pulpperf.reporting.tasks_service_time(results))
